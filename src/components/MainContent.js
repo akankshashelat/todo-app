a74abd4df4bdea78c9f671c8d427e825affe4d12
Akanksha Shelat <akankshashelat12@gmail.com>
2019-03-11 17:50:24 -0400

    created a mini to-do list with no design yet

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,7 +4,12 @@ function MainContent(){
     return(
         <div>
             <main>
-                <p>This is all the main content of the page </p>
+                {/* going to hard code some things in my list. */}
+                {/* need checkboxes next to each thing in the list. */}
+                <input type="checkbox" /> Fold my clothes <br/>
+                <input type="checkbox" /> Make food <br/>
+                <input type="checkbox" /> Call mother! <br/>
+                <input type="checkbox" /> Do the dishes :( <br/>
             </main>
         </div>
     )
