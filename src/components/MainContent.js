c8ea58de6d366d277e3cfc617d4566dfae730312
Akanksha Shelat <akankshashelat12@gmail.com>
2019-04-03 19:12:06 -0400

    state and onChange handling

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,18 +2,48 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function MainContent(){
-    //have to convert array of todosData into array of TodoItem
-    //use map method! 
 
-    //for each thing => return a TodoItem with an object called item that has a ={thing} inside it.
-    const todoItems = todosData.map(eachThing => <TodoItem key = {eachThing.id} item = {eachThing}/>);
-    return(
-        <main className="todo-list">
-            {/* need checkboxes next to each thing in the list. */}
-            {todoItems}
-        </main>
+//changing the function to a class with a state.
+class MainContent extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            todoThings: todosData}
         
-    )
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    handleChange(id){
+
+        this.setState(prevState => {
+            const updated = prevState.todoThings.map(
+                eachThing => {
+                    if(eachThing.id === id){
+                        eachThing.completed = !eachThing.completed
+                    }
+                    return eachThing})
+
+            return{todoThings: updated}})
+                     
+    }
+    render(){
+        //have to convert array of todosData into array of TodoItem
+        //use map method! 
+        //for each thing => return a TodoItem with an object called item that has a ={thing} inside it.
+        const todoItems = this.state.todoThings.map(eachThing => 
+        <TodoItem key = {eachThing.id} 
+        item = {eachThing} 
+        handleChange = {this.handleChange}/>);
+
+        return(
+            <main className="todo-list">
+                {/* need checkboxes next to each thing in the list. */}
+                {todoItems}
+            </main>
+            
+        )
+    }
 }
 export default MainContent 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,12 @@ import React from "react";
 function TodoItem (props){
     return(
     <form className = "todo-item">
-        <input type="checkbox"/> {props.item.text} <br/>
+        <input 
+        type="checkbox" 
+        checked ={props.item.completed} 
+        onChange={() => props.handleChange(props.item.id)}
+        /> 
+        {props.item.text} 
     </form>
     )
 }
